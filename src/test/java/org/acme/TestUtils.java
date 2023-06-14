8ba72c91223eb19d502e3a6ebf42b1d43755e070
KatKentr <kkentroti@gmail.com>
2023-06-14 15:43:13 +0300

    methods that return a mock of type user Added

--- src/test/java/org/acme/TestUtils.java
+++ src/test/java/org/acme/TestUtils.java
@@ -2,6 +2,8 @@ package org.acme;
 
 import org.acme.model.User;
 
+import static org.mockito.Mockito.mock;
+
 public class TestUtils {
 
 
@@ -32,8 +34,35 @@ public class TestUtils {
         user.setEmail("test2@example.com");
         user.setPassword("1234");
         user.setRole("ADMIN");
-        user.setId(1L);
+        user.setId(2L);
 
+        return user;
+
+    }
+
+    //The methods return a mock of type User
+
+    public User createValidUserMockwithRoleAdmin(){
+
+        user =mock(User.class);                 //initialize a valid user
+        user.setUsername("Testperson2");
+        user.setEmail("test2@example.com");
+        user.setPassword("1234");
+        user.setRole("ADMIN");
+        user.setId(2L);
+
+        return user;
+
+    }
+
+    public User createValidUserMockwithRoleUser(){
+
+        user =mock(User.class);                 //initialize a valid user
+        user.setUsername("Testperson");
+        user.setEmail("test@example.com");
+        user.setPassword("1234");
+        user.setRole("USER");
+        user.setId(1L);
         return user;
 
     }
