33ce95f3689f86762be0bbfeb71cd0cd04f76701
Katerina <kkentroti@gmail.com>
2023-10-02 21:56:11 +0300

    tags and useful comments added

--- src/main/java/org/acme/MoviePlatformApiApplication.java
+++ src/main/java/org/acme/MoviePlatformApiApplication.java
@@ -5,10 +5,16 @@ import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
 import org.eclipse.microprofile.openapi.annotations.info.Contact;
 import org.eclipse.microprofile.openapi.annotations.info.Info;
 import org.eclipse.microprofile.openapi.annotations.info.License;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
+//OpenAPI: defines a standard standard, language-agnostic interface to RESTful APIs which allows both humans and computers to discover and understand the capabilities of the service without access to source code, documentation, or through network traffic inspection.
 //This Jakarta REST Application class was created to provide global API information by using OpenAPI annotations
-
+//Check the APIs operations and schemas http://localhost:8080/q/swagger-ui/
 @OpenAPIDefinition(
+        tags = {
+                @Tag(name="movies", description="Movie operations"),
+                @Tag(name="users", description="User operations")
+        },
         info = @Info(
                 title="Movie Platform API",
                 version = "1.0.0",
